40369dba0c40f12767feb94b031d6ea50ea01672
ShaunakMishra25 <shaunakmishra374@gmail.com>
2025-06-20 23:51:06 +0530

    Fix: Added types for html2pdf.js and improved PDF download

--- src/types/html2pdf.d.ts
+++ src/types/html2pdf.d.ts
@@ -1,7 +1,19 @@
 declare module "html2pdf.js" {
-  const html2pdf: () => {
-    set: (options: any) => any;
-    from: (element: HTMLElement) => { save: () => void };
-  };
+  interface Html2PdfOptions {
+    margin?: number | number[];
+    filename?: string;
+    image?: { type?: string; quality?: number };
+    html2canvas?: { scale?: number };
+    jsPDF?: { unit?: string; format?: string | number[]; orientation?: string };
+  }
+
+  interface Html2PdfInstance {
+    set: (opt: Html2PdfOptions) => Html2PdfInstance;
+    from: (element: HTMLElement) => Html2PdfInstance;
+    save: () => void;
+  }
+
+  function html2pdf(): Html2PdfInstance;
+
   export default html2pdf;
 }
